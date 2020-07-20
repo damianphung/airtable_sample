18a68f14d7e725aa8efe90425eca6b55dd0625a1
Damian Phung <damianphung@gmail.com>
2020-07-20 14:21:44 +1000

    fix

--- client/client.js
+++ client/client.js
@@ -1,6 +1,5 @@
+// If this were on a different host, need to specify full URL to the backend to process request
 const API_URL = 'http://localhost:5001/login/facebook';
-const form = document.querySelector('form');
-
 
 function fbLogin()
 {
@@ -12,16 +11,3 @@ function fbLogin()
         // }
     });
 }
-
-form.addEventListener('submit', (event) => {
-    // on form, it attempts to send the data. Lets prevent this default
-    event.preventDefault();
-
-    fetch(API_URL, {
-        method: 'GET',
-        // body: JSON.stringify(mew),
-        // headers: {
-        //     'content-type' : 'application/json'
-        // }
-    });
-});
